2d18f6c94d2e96fbea97a714c6ee505800bb8326
Kat Inskip <kat@inskip.me>
2025-07-24 20:13:07 -0700

    feat: katgba

--- flake.nix
+++ flake.nix
@@ -19,10 +19,20 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
+    katgba = {
+      url = "github:kittywitch/katgba/main";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "flake-compat";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = {
+    self,
     flake-utils,
+    katgba,
     nixpkgs,
     nur,
     ...
@@ -34,9 +44,16 @@
         overlays = [
           nur.overlays.default
           overlays.default
+          (final: prev: {
+              katgba = katgba.packages.${system}.default;
+          })
         ];
       };
       devShells.default = import ./shell.nix {inherit pkgs;};
+      packages = {
+        katgba = pkgs.katgba;
+        fetchdps = pkgs.fetchdps;
+      };
     });
     formatting = import ./formatting.nix {inherit inputs;};
   in
